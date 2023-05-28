3b40f1a87e8acb5b29560bc37b5dd442712c26e4
AntonShorin <Antnshorin2007@gmail.com>
2023-05-28 19:50:43 +0300

    Работает

---
+++ src/main/java/web/controllers/HelloController.java
@@ -0,0 +1,23 @@
+package web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HelloController {
+
+    @GetMapping(value = "/")
+    public String printWelcome(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Привет!");
+        messages.add("Моё первое Spring MVC приложение");
+        messages.add("Добро пожаловать! ");
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+
+}
